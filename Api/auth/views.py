e7361fd5f6c6fc5f5c27944b7f3872b2545e79cf
Olajiive <muizolatunji29@gmail.com>
2023-03-08 21:22:15 -0800

    added signup and login authentication

--- Api/auth/views.py
+++ Api/auth/views.py
@@ -1,14 +1,61 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource,fields
+from flask import request
+from flask_jwt_extended import create_access_token, create_refresh_token
+from ..models.users import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from http import HTTPStatus
 
 auth_namespace =Namespace("auth", description= "name space for authentication")
 
+signup_model =auth_namespace.model(
+    "Signup", {
+        "id":fields.Integer(),
+        "username":fields.String(required=True, description="A username"),
+        "email":fields.String(required=True, description="An email"),
+        "password":fields.String(required=True, description="A passord")
+    }
+)
+
+user_model =auth_namespace.model(
+    "User", {
+        "id":fields.Integer(),
+        "username":fields.String(required=True, description="A username"),
+        "email":fields.String(required=True, description="An email"),
+        "password_hash":fields.String(required=True, description="A passord"),
+        "is_active": fields.Boolean(description='This shows that User is active or not'),
+        "is_staff":fields.Boolean(description="This shows that User ia a staff or not")
+    }
+)
+
+login_model =auth_namespace.model(
+    "Login", {
+        "email":fields.String(required=True, description="An email"),
+        "password":fields.String(required=True, description="A passord")
+    }
+)
+
 @auth_namespace.route("/signup")
 class Signup(Resource):
+
+    @auth_namespace.expect(signup_model)
+    @auth_namespace.marshal_with(user_model)
     def post(self):
         """
             Signup a user
         """
-        pass
+        data=request.get_json()
+
+        new_user=User(
+            username=data.get("username"),
+            email=data.get("email"),
+            password_hash=generate_password_hash(data.get("password"))                
+        )
+        
+        new_user.save()
+
+        return new_user, HTTPStatus.CREATED
+        
+        
 
 @auth_namespace.route("/login")
 class Login(Resource):
@@ -16,5 +63,21 @@ class Login(Resource):
         """
             Generate JWT token
         """
-        pass
+        data = request.get_json()
+
+        email=data.get("email")
+        password=data.get("password")
+        user= User.query.filter_by(email=email).first()
+
+        if (user is not None) and check_password_hash(user.password_hash, password):
+            access_token=create_access_token(identity=user.username)
+            refresh_token=create_refresh_token(identity=user.username)
+
+            response = {
+                "acces_token": access_token,
+                "refresh_token": refresh_token
+            }
+
+            return response, HTTPStatus.CREATED
+
 
